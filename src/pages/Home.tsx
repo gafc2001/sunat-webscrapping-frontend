a0c3461db54787f6c17c9eb39fe4594a0195b703
Julian Quispe <julianmql@hotmail.com>
2024-06-20 03:47:29 -0500

    feat: abstraction of components added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,33 @@
-import { Container } from "@mantine/core"
-
-import { Busqueda } from "../components/Busqueda";
 import { GlobalContext, GlobalType, useGlobaState } from "../context/GlobalState";
 import { RazonSocial, RazonSocialModel } from "../components/RazonSocial";
+import { Header } from "../components/Header/Header";
+import { HeroBackground } from "../components/HeroBackground/HeroBackground";
+import { HeroSearch } from "../components/HeroSearch/HeroSearch";
+import { Footer } from "../components/Footer/Footer";
 
 export const Home = () => {
 
     const {appData} = useGlobaState();
 
-    console.log(appData.resultados_busqueda.map)
-    console.log(!!appData.resultados_busqueda)
+    //console.log(appData.resultados_busqueda.map)
+    //console.log(!!appData.resultados_busqueda)
 
-    return <Container>
-        <div>
-            <Busqueda/>
-            {!!appData?.resultados_busqueda && 
-            <div>
-                <h2>Resultados (Coincidencias {appData.resultados_busqueda.length} )</h2>
-                <div>
-                    {appData?.resultados_busqueda.map( (el : RazonSocialModel,ind : number ) => {
-                        return <RazonSocial key={ind} razonSocial={el}/>
-                    })}
-                </div>
-            </div>
-            }
-        </div>
-    </Container>
+    return <>
+        <Header/>
+        <HeroBackground/>
+        <HeroSearch/>
+        {
+        //{!!appData?.resultados_busqueda && 
+        //<div>
+        //    <h2>Resultados (Coincidencias {appData.resultados_busqueda.length} )</h2>
+        //    <div>
+        //        {appData?.resultados_busqueda.map( (el : RazonSocialModel,ind : number ) => {
+        //            return <RazonSocial key={ind} razonSocial={el}/>
+        //        })}
+        //    </div>
+        //</div>
+        //}
+        }
+        <Footer/>
+    </>;
 }
