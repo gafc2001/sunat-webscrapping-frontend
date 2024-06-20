794dd83ca110ba443cdcb8d3636e5e08f9fd50fb
Gustavo Farfan <farfancoraje15@gmail.com>
2024-06-20 13:18:07 -0500

    router & home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { MantineProvider } from '@mantine/core';
 import { Home } from './pages';
 import { GlobalState } from './context/GlobalState';
 import { ModalProvider } from './context/AppModal';
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <MantineProvider>
       <GlobalState>
         <ModalProvider>
-          <Home/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+          </Routes>
         </ModalProvider>
       </GlobalState>
     </MantineProvider>
   )
 }
+
 
 export default App
