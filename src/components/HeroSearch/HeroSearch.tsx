b9e3adc6190b3c7e41d6f2f308d235af1580dda1
Julian Quispe <julianmql@hotmail.com>
2024-06-20 12:17:41 -0500

    feat: historial block added

--- src/components/HeroSearch/HeroSearch.tsx
+++ src/components/HeroSearch/HeroSearch.tsx
@@ -1,5 +1,6 @@
-import { Center, Container, Flex, NativeSelect, Text, TextInput, Title } from '@mantine/core';
+import { Button, Center, Container, Flex, Group, NativeSelect, Text, TextInput, Title } from '@mantine/core';
 import classes from './HeroSearch.module.css';
+import { IconHistory } from '@tabler/icons-react';
 
 
 export function HeroSearch() {
@@ -33,19 +34,42 @@ export function HeroSearch() {
             wrap="wrap"
         >
         <NativeSelect 
-            w={{ base: 300, md: 600, lg: 700 }}
+            w={{ base: 200, md: 400, lg: 400 }}
             label="Seleccione el tipo de búsqueda" 
             data={['RUC', 'DNI', 'Carnet de Extranjería', 'Pasaporte', 'Ced. Diplomática de Identidad', 'Nombre/Razon Social']} 
             radius="xl"
             size="md"
         />
         <TextInput
-            w={{ base: 300, md: 600, lg: 700 }}
+            w={{ base: 200, md: 400, lg: 400 }}
             radius="xl"
             size="md"
             label="Ingrese el numero del documento seleccionado"
             placeholder="N° de documento"
         />
+        </Flex>
+        <Flex
+            m={20}
+            mih={50}
+            gap="lg"
+            justify="center"
+            align="center"
+            direction="column"
+            wrap="wrap"
+        >
+          <Title order={3}>Historial</Title>
+          <Button
+            mih={50}
+            component="a"
+            size="sm"
+            leftSection={<IconHistory style={{ width: '1rem', height: '1rem' }} color="#00ACEE" />}
+            variant="default"
+          >
+            <Group gap="sm">
+              <Title order={5}>Konecta</Title>
+              <Text>RUC: 101010020</Text>
+            </Group>
+          </Button>
         </Flex>
     </Flex>
     </Container>
